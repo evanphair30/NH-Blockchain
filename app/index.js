fc5d4c20c183e35a8bae2a4016e64f4b667bd320
Evan Phair <50122734+evanphair30@users.noreply.github.com>
2019-06-08 13:43:07 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,28 +4,30 @@ const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
+const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
-const bc= new Blockchain();
+const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
-const p2pServer= new P2pServer(bc, tp);
+const p2pServer = new P2pServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(bodyParser.json());
 
-app.get('/blocks', (req, res) =>{
+app.get('/blocks', (req, res) => {
   res.json(bc.chain);
 });
 
 app.post('/mine', (req, res) => {
-const block = bc.addBlock(req.body.data);
-console.log(`New block added: ${block.toString()}`);
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
 
-p2pServer.syncChains();
+  p2pServer.syncChains();
 
-res.redirect('/blocks');
+  res.redirect('/blocks');
 });
 
 app.get('/transactions', (req, res) => {
@@ -34,14 +36,20 @@ app.get('/transactions', (req, res) => {
 
 app.post('/transact', (req, res) => {
   const { recipient, amount } = req.body;
-  const transaction = wallet.createTransaction(recipient, amount, tp);
+  const transaction = wallet.createTransaction(recipient, amount, bc, tp);
   p2pServer.broadcastTransaction(transaction);
   res.redirect('/transactions');
+});
+
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine();
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
 });
 
 app.get('/public-key', (req, res) => {
   res.json({ publicKey: wallet.publicKey });
 });
 
-app.listen(HTTP_PORT, ()=> console.log(`Listening on port ${HTTP_PORT}`));
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pServer.listen();
